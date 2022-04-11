ee89ead982785adb0632382842f27214523b75df
valju <juhani.valimaki@haaga-helia.fi>
2022-04-11 11:45:57 +0300

    Database - editing sql scripts

--- Database/SQL_Scripts/01_create_tables.sql
+++ Database/SQL_Scripts/01_create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `Order` 
 (
-    uuid        CHAR(24)        NOT NULL,
+    uuid        CHAR(24)        UNIQUE NOT NULL,
     pu_address  VARCHAR(255)    NOT NULL,
 
     CONSTRAINT pk_Order PRIMARY KEY(uuid) 
