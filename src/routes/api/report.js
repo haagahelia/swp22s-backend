06df181cd2519e74942af3cd28b732ef23b8f603
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-04-15 11:40:00 +0300

    added GET SIGNED TASKS, GET NOT SIGNED TASKS

--- src/routes/api/report.js
+++ src/routes/api/report.js
@@ -23,4 +23,26 @@ report.get("/", (req, res) => {
         })
 })
 
+// GET SIGNED http:localhost:8777/api/report/signed
+report.get("/signed", (req, res) => {
+    knex("Task").select().whereNotNull("pu_signed_at")
+        .then((signatureArray) => {
+            successHandler(res, signatureArray, "GET all signed tasks worked!")
+        })
+        .catch((error) => {
+            databaseErrorHandler(res, error, "Could not get the signed signatures from DB!")
+        })
+})
+
+// GET NOT SIGNED http:localhost:8777/api/report/notsigned
+report.get("/notsigned", (req, res) => {
+    knex("Task").select().whereNull("pu_signed_at")
+        .then((signatureArray) => {
+            successHandler(res, signatureArray, "GET all not signed tasks worked!")
+        })
+        .catch((error) => {
+            databaseErrorHandler(res, error, "Could not get the not signed signatures from DB!")
+        })
+})
+
 export default report;
