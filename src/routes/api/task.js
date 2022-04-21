f620fe9c53fda41b23bddf4a672c2bf1b39d07a5
valju <juhani.valimaki@haaga-helia.fi>
2022-04-21 12:22:47 +0300

    Lot of fixes in Thu 2022-04-21 check and debug sessions. See videos. Still some small probs

--- src/routes/api/task.js
+++ src/routes/api/task.js
@@ -34,42 +34,42 @@ task.get("/:uuid", (req, res) => {
 // This endpoint is for entering tech test data. A new endpoint should be added:
 // Where all the needed data is in, BUT no pu_signature_image NOR pu_signed_at yet.   TODO
 //SAVE TO DB POST http:localhost:8787/api/task   
-task.post("/", async (req, res) => {
-    //Note req.files not req.body!
-    const files = req.files;
-    if (!files) {
-        requestErrorHandler(res, 400, "Request error. Data not found.");
-    } else if (!files.signature) {
-        requestErrorHandler(res, 400, "Signature not found in file");
+task.post("/", (req, res) => {
+    const body = req.body;
+    if (!req.body) {
+        requestErrorHandler(res, 400, "Request error. Task data body not found.");
+    } else if (!req.body.uuid || !req.body.pu_address) {
+        requestErrorHandler(res, 400, "uuid or address not found in request body.");
     } else {
-        try {
-            const image = files.signature;
-            const filecontents = readFileSync(image.tempFilePath).toString();
-            const rowIdArr = await knex
-                .insert({ image: filecontents })
-                .into("Signature")
-            successHandler(res, rowIdArr, `Signature successfully saved, inserted row id: ${rowIdArr}`, 201)
-        } catch (err) {
-            databaseErrorHandler(res, err);
-        }
+        knex.insert(body).into("Task")
+        .then((rowIdArr) => {
+            successHandler(res, rowIdArr, `Task successfully saved, id not yet used: ${rowIdArr}`)
+        })
+        .catch( (err) => {
+            databaseErrorHandler(res, err, 
+                `Inserting new task with ${req.body.uuid} failed`)
+        })    
     }
 })
 
-//DELETE ONE BY ID http:localhost:8787/api/task/:id
-task.delete("/:id", async (req, res) => {
-    if (!req.params.id) {
-        requestErrorHandler(res, 400, "Signature id is missing.");
+//DELETE ONE BY ID http:localhost:8787/api/task/:uuid
+task.delete("/:uuid", (req, res) => {
+    if (!req.params.uuid) {
+        requestErrorHandler(res, "Task uuid is missing.");
     } else {
-        try {
-            const rowsAffected = await knex("Signature").where("uuid", req.params.id).del()
+        knex("Task").where("uuid", req.params.uuid).del()
+        .then( (rowsAffected) => {
             if (rowsAffected === 1) {
-                successHandler(res, rowsAffected, `Successfully deleted signature, modified rows: ${rowsAffected}.`, 204);
+                successHandler(res, rowsAffected, `Successfully deleted task, modified rows: ${rowsAffected}.`);
             } else {
-                requestErrorHandler(res, 404, `Signature with id: ${req.params.id} not found.`);
+                requestErrorHandler(res, `Signature with id: ${req.params.id} not found.`);
             }
-        } catch (error) {
-            databaseErrorHandler(res, error)
-        }
+        })
+        .catch( (error) => {
+            databaseErrorHandler(res, error);
+            
+            //, `Deleting task with uuid ${req.params.uuid} failed.`
+        })
     }
 })
 
