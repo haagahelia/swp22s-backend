1cab3470891393d49e9c22bd1a0cd52bbb6d851c
Vincent Le <vincentle.fh@gmail.com>
2022-04-24 21:11:30 +0300

    Minor fix to PUT route

--- src/routes/api/signature.js
+++ src/routes/api/signature.js
@@ -14,25 +14,26 @@ const signature = express.Router();
 // This router is about signing the tasks (after pickup and later after delivery)
 
 //UPDATE ONE BY ID PUT http:localhost:8787/api/signature/:id
-signature.put("/:id", (req, res) => {
-  const files = req.files;
-  if (!files) {
-    requestErrorHandler(res, "400 - Request error. Data not found.");
-  } else {
-      const image = files.signature;
-      const fileContents = readFileSync(image.tempFilePath).toString("base64");
-      console.log(fileContents)
-      const rowsAffected = await knex("Task")
-        .where({ uuid: req.params.id })
-        .update({ pu_signature_image: fileContents })
-      if (rowsAffected === 1) {
-        successHandler(res, String(rowsAffected), `Successfully updated signature, modified rows: ${rowsAffected}.`)
-      } else {
-        requestErrorHandler(res, `409 - Duplicated: signature with id ${req.params.id}.`)
-      }
-    } catch (error) {
-      databaseErrorHandler(res, error)
+signature.put("/:id", async (req, res) => {
+  try {
+    const files = req.files;
+    if (!files) {
+      requestErrorHandler(res, "400 - Request error. Data not found.");
+    } else {
+        const image = files.signature;
+        const fileContents = readFileSync(image.tempFilePath).toString("base64");
+        console.log(fileContents)
+        const rowsAffected = await knex("Task")
+          .where({ uuid: req.params.id })
+          .update({ pu_signature_image: fileContents })
+        if (rowsAffected === 1) {
+          successHandler(res, String(rowsAffected), `Successfully updated signature, modified rows: ${rowsAffected}.`)
+        } else {
+          requestErrorHandler(res, `409 - Duplicated: signature with id ${req.params.id}.`)
+        }
     }
+  } catch (error) {
+      databaseErrorHandler(res, error)
   }
 })
 
