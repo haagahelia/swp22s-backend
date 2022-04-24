97dbef5f3ea0919c0c83f5dc2da60265caea6662
Vincent Le <vincentle.fh@gmail.com>
2022-04-24 20:56:28 +0300

    Fix PUT route

--- src/routes/api/signature.js
+++ src/routes/api/signature.js
@@ -16,15 +16,14 @@ signature.put("/:id", async (req, res) => {
   const files = req.files;
   if (!files) {
     requestErrorHandler(res, "400 - Request error. Data not found.");
-  } else if (!req.params.id) {
-    requestErrorHandler(res, "400 - Signature id is missing.");
   } else {
     try {
       const image = files.signature;
-      const fileContents = readFileSync(image.tempFilePath).toString();
-      const rowsAffected = await knex("Signature")
-        .where({ id: req.params.id })
-        .update({ image: fileContents })
+      const fileContents = readFileSync(image.tempFilePath).toString("base64");
+      console.log(fileContents)
+      const rowsAffected = await knex("Task")
+        .where({ uuid: req.params.id })
+        .update({ pu_signature_image: fileContents })
       if (rowsAffected === 1) {
         successHandler(res, String(rowsAffected), `Successfully updated signature, modified rows: ${rowsAffected}.`)
       } else {
