2e83a38bd74be5cddd94a82ffdd7f5e04a1483f7
Chevrolet Anthony <bgt598@myy.haaga-helia.fi>
2022-05-05 19:13:08 +0300

    Add endpoint get all countries and order type for select menu in the front

---
+++ src/routes/api/orderType.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import knex from "../../db/index.js"
+import {
+    successHandler,
+    databaseErrorHandler
+} from "../../responseHandlers/index.js"
+
+const orderType = express.Router();
+
+// This router is for get all order type 
+
+// GET ALL http:localhost:8777/api/report/
+orderType.get("/", (req, res) => {
+    knex("OrderType").select()
+        .then((signatureArray) => {
+            successHandler(res, signatureArray, "GET all Order Type !")
+        })
+        .catch((error) => {
+            databaseErrorHandler(res, error, "Could not get the Order Type from DB!")
+        })
+})
+
+export default orderType;
